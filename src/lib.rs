315c17cb2361f48bd2b3e75c0f37eb221e48cc61
wangyuntao <wyt.daily@gmail.com>
2019-07-28 20:23:14 +0800

    155. Min Stack

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,4 @@ pub mod p0100_same_tree;
 pub mod p0101_symmetric_tree;
 pub mod p0104_maximum_depth_of_binary_tree;
 pub mod p0121_best_time_to_buy_and_sell_stock;
+pub mod p0155_min_stack;
---
+++ src/p0155_min_stack.rs
@@ -0,0 +1,59 @@
+pub struct MinStack {
+    v: Vec<i32>,
+    m: Option<i32>,
+}
+
+impl MinStack {
+    /** initialize your data structure here. */
+    pub fn new() -> Self {
+        MinStack { v: vec![], m: None }
+    }
+
+    pub fn push(&mut self, x: i32) {
+        self.v.push(x);
+        if let Some(m) = self.m {
+            if x < m {
+                self.m = Some(x);
+            }
+        }
+    }
+
+    pub fn pop(&mut self) {
+        let x = self.v.pop().unwrap();
+        if let Some(m) = self.m {
+            if x == m {
+                self.m = None;
+            }
+        }
+    }
+
+    pub fn top(&self) -> i32 {
+        return self.v[self.v.len() - 1];
+    }
+
+    pub fn get_min(&mut self) -> i32 {
+        self.m.unwrap_or_else(|| {
+            let m = self.v.iter().min().unwrap();
+            let m = *m;
+            self.m = Some(m);
+            m
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut s = MinStack::new();
+        s.push(-2);
+        s.push(0);
+        s.push(-3);
+        assert_eq!(s.get_min(), -3);
+        s.pop();
+        assert_eq!(s.top(), 0);
+        assert_eq!(s.get_min(), -2);
+    }
+}
